1159d41e932d19ba42297022b53c45fb6f4b1988
faqih-firdaus26 <firdausfaqih727@gmail.com>
2023-11-19 18:58:22 +0700

    (fixed) add to cart, list cart and delete cart features

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Cart;
 
 class FrontendController extends Controller
 {
@@ -20,8 +22,25 @@ class FrontendController extends Controller
         return view('pages.frontend.details', compact('product', 'recomendations'));
     }
 
+    public function cartAdd(Request $request, $id){
+        Cart::create([
+            'users_id' => Auth::user()->id,
+            'products_id' => $id,
+        ]);
+
+        return redirect('cart');
+    }
+
+    public function cartDelete(Request $request, $id){
+        $item = Cart::findorfail($id);
+        $item ->delete();
+        return redirect('cart');
+    }
+
     public function cart(Request $request) {
-        return view('pages.frontend.cart');
+        $carts = Cart::with(['product.galleries'])->where('users_id', Auth::user()->id)->get();
+
+        return view('pages.frontend.cart', compact('carts'));
     }
 
     public function success(Request $request) {
